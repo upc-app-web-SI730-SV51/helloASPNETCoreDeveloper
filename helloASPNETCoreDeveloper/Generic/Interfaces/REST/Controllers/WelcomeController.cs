e797c70c899edc972019229e7d36a456e8a4e18f
Andrés Alberto Torres García <andresalbertotorresgarcia@gmail.com>
2024-08-24 11:41:52 -0500

    feat(welcome): added welcome inbound service

---
+++ helloASPNETCoreDeveloper/Generic/Interfaces/REST/Controllers/WelcomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace helloASPNETCoreDeveloper.Generic.Interfaces.REST.Controllers;
+
+[ApiController]
+[Route(template:"[controller]")]
+public class WelcomeController : ControllerBase
+{
+    
+    [HttpGet]
+    public string Welcome()
+    {
+        return "Welcome!";
+    }
+}
