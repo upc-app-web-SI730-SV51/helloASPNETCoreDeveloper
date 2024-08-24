3c3e0079e385fe214b7a5d55c5aad44d4682571c
Andrés Alberto Torres García <andresalbertotorresgarcia@gmail.com>
2024-08-24 11:46:48 -0500

    feat(welcome): added welcome builder entitie

---
+++ helloASPNETCoreDeveloper/Generic/Domain/Model/Entities/WelcomeBuilder.cs
@@ -0,0 +1,9 @@
+﻿namespace helloASPNETCoreDeveloper.Generic.Domain.Model.Entities;
+
+public class WelcomeBuilder
+{
+    public static string Builder(string? name) // existe la posibilidad de que el parametro puede ser numero
+    {
+        return name == null ? "Welcome!" : $"Welcome {name}!"; // si es null o entra un nombre (string)
+    }
+}
