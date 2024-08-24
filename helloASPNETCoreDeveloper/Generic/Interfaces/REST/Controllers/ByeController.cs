6f0de44579c0bcd4c4b2ff90c121f7415f392146
Andrés Alberto Torres García <andresalbertotorresgarcia@gmail.com>
2024-08-24 12:03:34 -0500

    feat(bye): added by inbound service.

---
+++ helloASPNETCoreDeveloper/Generic/Interfaces/REST/Controllers/ByeController.cs
@@ -0,0 +1,15 @@
+﻿using helloASPNETCoreDeveloper.Generic.Interfaces.REST.Resources;
+using Microsoft.AspNetCore.Mvc;
+
+namespace helloASPNETCoreDeveloper.Generic.Interfaces.REST.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ByeController: ControllerBase
+{
+    [HttpGet("{name}")]
+    public ByeResource Bye(string name)
+    {
+        return new ByeResource(name);
+    }
+}
