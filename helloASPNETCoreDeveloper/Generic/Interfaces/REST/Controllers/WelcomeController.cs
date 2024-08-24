27f1ccb315dec0a1ef7bf1dff4b56dc5d6a4d60f
Andrés Alberto Torres García <andresalbertotorresgarcia@gmail.com>
2024-08-24 12:02:46 -0500

    feat(welcome): added welcome action with name parameter.

--- helloASPNETCoreDeveloper/Generic/Interfaces/REST/Controllers/WelcomeController.cs
+++ helloASPNETCoreDeveloper/Generic/Interfaces/REST/Controllers/WelcomeController.cs
@@ -1,15 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using helloASPNETCoreDeveloper.Generic.Domain.Model.Entities;
+using Microsoft.AspNetCore.Mvc;
 
 namespace helloASPNETCoreDeveloper.Generic.Interfaces.REST.Controllers;
 
 [ApiController]
-[Route(template:"[controller]")]
+[Route("[controller]")]
 public class WelcomeController : ControllerBase
 {
     
     [HttpGet]
     public string Welcome()
     {
-        return "Welcome!";
+        return WelcomeBuilder.Builder(null);
     }
+    
+    [HttpGet("{name}")]
+    public string Welcome(string name)
+    {
+        return WelcomeBuilder.Builder(name);
+    }
+    
+    
 }
